2272bca6563099f3400469224e4cf5ecf69d1b45
Fanhao Yang <fyang01@college.harvard.edu>
2015-10-01 23:11:08 -0400

    think finaly got all gitignore

--- lab2_alu/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/cs141/Desktop/CS141-Labs/lab2_alu/add_rca_32_bit_test_isim_beh.exe" -prj "C:/Users/cs141/Desktop/CS141-Labs/lab2_alu/add_rca_32_bit_test_beh.prj" "work.add_rca_32_bit_test" "work.glbl" 
