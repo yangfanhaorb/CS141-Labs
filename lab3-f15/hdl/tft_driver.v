359144ccb139a2226809b39bb3815397dd4b9795
Fanhao Yang <yangfanhaorb@gmail.com>
2015-10-12 16:16:19 -0400

    added looping through x and y

--- lab3-f15/hdl/tft_driver.v
+++ lab3-f15/hdl/tft_driver.v
@@ -22,7 +22,29 @@ wire [(`TFT_BITS_PER_COLOR-1):0] r, g, b;
 pwm #(.N(8)) PWM0 (.clk(tft_clk), .rst(rst), .din(brightness), .dout(tft_backlight));
 //assign tft_backlight = 1; //comment out the pwm module instantiation and uncomment this line to check your tft driver module without PWM
 /* insert your code here <<< */
+always @(posedge tft_clk) begin
+	if(rst) begin
+		x <= 0;
+		y <= 0;
+	end
+	else begin
+		if(x < `TFT_X_RES + `TFT_X_BLANKING) begin
+			x <= x + 1;
+		end
+		else begin
+			x <= 0;
+			if (y < `TFT_Y_RES + `TFT_Y_BLANKING) begin
+				y <= y + 1;
+			end
+			else begin
+				y <= 0;
+			end
+		end
+	end
+end
 
+assign tft_data_ena = (x<`TFT_X_RES)|(y<`TFT_Y_RES);
+assign new_frame = (y==0)&(x==0);
 /* >>> */
 endmodule
 `default_nettype wire
